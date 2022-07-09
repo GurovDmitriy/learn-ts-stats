bb80cd36e3d8382faeb165f7f9854b3875ed6a5c
Gurov Dmitriy <gurovdmitriy1991@gmail.com>
2022-07-09 22:38:23 +0300

    feat: enum example

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,21 @@ const matches = fs
     return row.split(",")
   })
 
-console.log(matches[0])
+// enum - enumeration
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
+
+let manUnitedWins = 0
+
+for (const match of matches) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+    manUnitedWins += 1
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    manUnitedWins += 1
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`)
