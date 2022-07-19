ad3720ce9f5a8c855977c6d6f4cc6e2736f9e406
Gurov Dmitriy <gurovdmitriy1991@gmail.com>
2022-07-19 20:32:26 +0300

    feat: base for composition & inheritance

--- src/inheritance/CSVFileReader.ts
+++ src/inheritance/CSVFileReader.ts
@@ -1,9 +1,11 @@
 import fs from "fs"
 
-export class CSVFileReader {
-  data: string[][] = []
+export abstract class CSVFileReader<T> {
+  data: T[] = []
 
   constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): T
 
   read(): void {
     this.data = fs
@@ -14,5 +16,6 @@ export class CSVFileReader {
       .map((row: string): string[] => {
         return row.split(",")
       })
+      .map(this.mapRow)
   }
 }
